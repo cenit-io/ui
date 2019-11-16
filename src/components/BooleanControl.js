99f7f0a3369be0fb8974660178629b95c651c2c1
Maikel Arcia Carrazana <macarci@gmail.com>
2019-11-16 17:34:44 -0300

    Update | Enhancing boolean control

--- src/components/BooleanControl.js
+++ src/components/BooleanControl.js
@@ -1,31 +1,48 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ClearIcon from '@material-ui/icons/Clear';
 import BlankIcon from '@material-ui/icons/IndeterminateCheckBox';
 import CheckedIcon from '@material-ui/icons/CheckBox';
 import UncheckedIcon from '@material-ui/icons/CheckBoxOutlineBlank';
-import {FormControl, IconButton, Input, InputAdornment, InputLabel} from "@material-ui/core";
+import { FormControl, IconButton, FilledInput, InputAdornment, InputLabel } from "@material-ui/core";
 
 const BooleanControl = ({ title, onChange, value, onDelete, disabled }) => {
-
+    const [focused, setFocused] = useState(false);
     const CheckIcon = value === undefined || value === null ? BlankIcon : (value ? CheckedIcon : UncheckedIcon);
+    const displayValue = String(value);
+
+    const handleChange = () => {
+        setFocused(true);
+        onChange(!value);
+    };
+
+    const handleDelete = () => {
+        setFocused(true);
+        onDelete();
+    };
 
     return (
         <FormControl variant="filled" fullWidth={true} disabled={disabled}>
             <InputLabel>{title}</InputLabel>
-            <Input disabled
-                   endAdornment={
-                       <InputAdornment position="end">
-                           <IconButton onClick={() => onChange(!value)} disabled={disabled}>
-                               <CheckIcon/>
-                           </IconButton>
-                           {
-                               !disabled && value !== undefined && value !== null &&
-                               <IconButton onClick={onDelete}>
-                                   <ClearIcon/>
-                               </IconButton>
-                           }
-                       </InputAdornment>
-                   }
+            <FilledInput key={displayValue}
+                         readOnly
+                         value={focused ? displayValue : ''}
+                         autoFocus={focused}
+                         endAdornment={
+                             <InputAdornment position="end">
+
+                                 <IconButton onClick={handleChange} disabled={disabled}>
+                                     <CheckIcon color={focused ? 'primary' : 'inherit'}/>
+                                 </IconButton>
+                                 {
+                                     !disabled && value !== undefined && value !== null &&
+                                     <IconButton onClick={handleDelete}>
+                                         <ClearIcon/>
+                                     </IconButton>
+                                 }
+                             </InputAdornment>
+                         }
+                         onFocus={() => setFocused(true)}
+                         onBlur={() => setFocused(false)}
             />
         </FormControl>
     );
