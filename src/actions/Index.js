7df800321904d7876da87c077e376ff25ebabdb4
Maikel Arcia Carrazana <macarci@gmail.com>
2019-11-22 07:48:25 -0300

    Update | Using React effects (List action)

--- src/actions/Index.js
+++ src/actions/Index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from '../components/Loading';
-import { Checkbox, withStyles } from "@material-ui/core";
+import { Checkbox, makeStyles, useTheme } from "@material-ui/core";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -61,91 +61,86 @@ const EnhancedTableHead = ({ props, onSelectAllClick, order, orderBy, numSelecte
     );
 }
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
         overflow: 'auto',
     }
-});
+}));
 
 const MinItemsPerPage = 5;
 
 const ItemsPerPage = [MinItemsPerPage, 10, 25];
 
-class Index extends React.Component {
+function Index({ dataType, height, selection, onSelect }) {
 
-    state = {
-        order: 'asc',
-        orderBy: 'id',
-        page: 0,
-        dense: false,
-        limit: MinItemsPerPage
-    };
+    const [props, setProps] = useState(null);
+    const [data, setData] = useState(null);
+    const [dense, setDense] = useState(false);
+    const [limit, setLimit] = useState(MinItemsPerPage);
+    const [order, setOrder] = useState('asc');
+    const [orderBy, setOrderBy] = useState('_id');
+    const [page, setPage] = useState(0);
+    const classes = useStyles();
+    const theme = useTheme();
 
-    computeDataTypeState = () => {
-        const { dataType } = this.props;
-
-        return dataType.queryProps().pipe(
+    useEffect(() => {
+        const subscription = dataType.queryProps().pipe(
             switchMap(
                 queryProps => zzip(...queryProps.map(prop => prop.getTitle())).pipe(
-                    map(titles => {
-                            const props = titles.map(
-                                (title, index) => ({ title, prop: queryProps[index] })
-                            );
-                            return { props };
-                        }
+                    map(
+                        titles => titles.map(
+                            (title, index) => ({ title, prop: queryProps[index] })
+                        )
                     )
                 )
             )
-        );
-    };
+        ).subscribe(props => setProps(props));
 
-    requestData = () => {
+        return () => subscription.unsubscribe();
+    }, [dataType]);
 
-        const { dataType } = this.props,
-
-            { limit, page } = this.state;
-
-        return dataType.find('', {
+    useEffect(() => {
+        const subscription = dataType.find('', {
             limit: limit,
             page: page,
             sort: { _id: -1 },
             props: dataType.queryProps()
-        });
+        }).subscribe(data => setData(data));
+
+        return () => subscription.unsubscribe();
+    }, [limit, page, dataType]);
+
+    const handleRequestSort = (event, property) => {
+        const isDesc = orderBy === property && order === 'desc';
+
+        setOrder(isDesc ? 'asc' : 'desc');
+        setOrderBy(property);
     };
 
-    handleRequestSort = (event, property) => {
-        const { order, orderBy } = this.state,
-            isDesc = orderBy === property && order === 'desc';
+    const select = selected => {
+        onSelect(selected);
+    }
 
-        this.setState({
-            order: isDesc ? 'asc' : 'desc',
-            orderBy: property
-        });
-    };
-
-    handleSelectAllClick = event => {
+    const handleSelectAllClick = event => {
         let selected;
         if (event.target.checked) {
-            const { data } = this.state;
             selected = data.items.map(item => item.id);
         } else {
             selected = [];
         }
-        this.select(selected);
+        select(selected);
     };
 
-    handleSelectOne = id => () => {
-        const { selection } = this.props;
+    const handleSelectOne = id => () => {
         if (selection.length === 1 && selection[0] === id) {
-            this.select([]);
+            select([]);
         } else {
-            this.select([id]);
+            select([id]);
         }
     }
 
-    handleSelect = id => () => {
-        const { selection } = this.props;
+    const handleSelect = id => () => {
         const index = selection.indexOf(id);
         const newSelection = [...selection];
 
@@ -155,135 +150,111 @@ class Index extends React.Component {
             newSelection.splice(index, 1);
         }
 
-        this.select(newSelection);
+        select(newSelection);
     };
 
-    select = selected => {
-        this.props.onSelect(selected);
-    }
-
-    handleChangePage = (event, page) => {
+    const handleChangePage = (event, page) => {
         page++;
-        this.setState({ page, data: null });
-        this.select([]);
+        setPage(page);
+        setData(null);
+        select([]);
     };
 
-    handleChangeRowsPerPage = (event) => {
-        this.setState({
-            limit: +event.target.value,
-            page: 0,
-            data: null
-        });
-        this.select([]);
+    const handleChangeRowsPerPage = event => {
+        setLimit(+event.target.value);
+        setPage(0);
+        setData(null);
+        select([]);
     };
 
-    handleChangeDense = (event) => {
-        this.setState({ dense: event.target.checked });
+    const handleChangeDense = event => {
+        setDense(event.target.checked);
     };
 
-    render() {
+    //const { order, orderBy, page} = this.state;
 
-        const { height, selection, classes, theme } = this.props;
+    //const isSelected = name => selection.indexOf(name) !== -1;
 
-        const { props, data, dense, limit } = this.state;
+    let tableHeight = height;
 
-        //const { order, orderBy, page} = this.state;
-
-        //const isSelected = name => selection.indexOf(name) !== -1;
-
-        let table, tableHeight = height;
-
-        if (props) {
-            if (data) {
-                /*const headCells = props.map(prop => <TableCell key={prop.prop.name}
-                                                               style={{
-                                                                   backgroundColor: "#fff",
-                                                                   position: "sticky",
-                                                                   top: 0
-                                                               }}>{prop.title}</TableCell>);*/
-
-                const rows = data.items.map(item => {
-                    let isSelected = selection.indexOf(item.id) !== -1;
-                    return <TableRow hover
-                                     key={item.id}
-                                     onClick={this.handleSelectOne(item.id)}
-                                     role="checkbox"
-                                     aria-checked={isSelected}
-                                     tabIndex={-1}>
-                        <TableCell padding="checkbox"
-                                   style={{
-                                       backgroundColor: "#fff",
-                                       position: "sticky",
-                                       left: 0,
-                                       zIndex: 2
-                                   }}>
-                            <Checkbox checked={isSelected}
-                                      inputProps={{ 'aria-labelledby': item.id }}
-                                      onChange={this.handleSelect(item.id)}/>
-                        </TableCell>
-                        {
-                            props.map(prop => (
-                                <TableCell key={`${item.id}.${prop.prop.name}`}>
-                                    {String(item[prop.prop.jsonKey])}
-                                </TableCell>
-                            ))
-                        }
-                    </TableRow>
-                });
-
-                let pagination;
-
-                if (data.count > MinItemsPerPage) {
-                    tableHeight = `${tableHeight} - ${theme.spacing(7)}px`;
-                    pagination = <TablePagination component='div'
-                                                  rowsPerPageOptions={ItemsPerPage}
-                                                  labelRowsPerPage='Page size'
-                                                  count={data.count}
-                                                  rowsPerPage={limit}
-                                                  page={data.current_page - 1}
-                                                  backIconButtonProps={{
-                                                      'aria-label': 'Previous Page',
-                                                  }}
-                                                  nextIconButtonProps={{
-                                                      'aria-label': 'Next Page',
-                                                  }}
-                                                  onChangePage={this.handleChangePage}
-                                                  onChangeRowsPerPage={this.handleChangeRowsPerPage}/>;
-                }
-
-                table = <React.Fragment>
-                    <div style={{ height: `calc(${tableHeight})`, overflow: 'auto' }}>
-                        <Table className={classes.table}
-                               size={dense ? 'small' : 'medium'}>
-                            <EnhancedTableHead props={props}
-                                               onSelectAllClick={this.handleSelectAllClick}
-                                               numSelected={selection.length}
-                                               rowCount={data.items.length}/>
-                            <TableBody>
-                                {rows}
-                            </TableBody>
-                        </Table>
-                    </div>
-                    {pagination}
-                </React.Fragment>;
-            } else {
-                this.requestData().subscribe(data => { //TODO sanitize with unsubscribe
-                    this.setState({ data })
-                });
-                table = <Loading height={`calc(${tableHeight})`}/>;
-            }
-        } else {
-            this.computeDataTypeState().subscribe( //TODO sanitize with unsubscribe
-                state => this.setState(state)
-            );
-            table = <Loading height={`calc(${tableHeight})`}/>;
-        }
-
-        return table;
+    if (!props || !data) {
+        return <Loading height={`calc(${tableHeight})`}/>;
     }
+
+    /*const headCells = props.map(prop => <TableCell key={prop.prop.name}
+                                                   style={{
+                                                       backgroundColor: "#fff",
+                                                       position: "sticky",
+                                                       top: 0
+                                                   }}>{prop.title}</TableCell>);*/
+
+    const rows = data.items.map(item => {
+        let isSelected = selection.indexOf(item.id) !== -1;
+        return <TableRow hover
+                         key={item.id}
+                         onClick={handleSelectOne(item.id)}
+                         role="checkbox"
+                         aria-checked={isSelected}
+                         tabIndex={-1}>
+            <TableCell padding="checkbox"
+                       style={{
+                           backgroundColor: "#fff",
+                           position: "sticky",
+                           left: 0,
+                           zIndex: 2
+                       }}>
+                <Checkbox checked={isSelected}
+                          inputProps={{ 'aria-labelledby': item.id }}
+                          onChange={handleSelect(item.id)}/>
+            </TableCell>
+            {
+                props.map(prop => (
+                    <TableCell key={`${item.id}.${prop.prop.name}`}>
+                        {String(item[prop.prop.jsonKey])}
+                    </TableCell>
+                ))
+            }
+        </TableRow>
+    });
+
+    let pagination;
+
+    if (data.count > MinItemsPerPage) {
+        tableHeight = `${tableHeight} - ${theme.spacing(7)}px`;
+        pagination = <TablePagination component='div'
+                                      rowsPerPageOptions={ItemsPerPage}
+                                      labelRowsPerPage='Page size'
+                                      count={data.count}
+                                      rowsPerPage={limit}
+                                      page={data.current_page - 1}
+                                      backIconButtonProps={{
+                                          'aria-label': 'Previous Page',
+                                      }}
+                                      nextIconButtonProps={{
+                                          'aria-label': 'Next Page',
+                                      }}
+                                      onChangePage={handleChangePage}
+                                      onChangeRowsPerPage={handleChangeRowsPerPage}/>;
+    }
+
+    return <React.Fragment>
+        <div style={{ height: `calc(${tableHeight})`, overflow: 'auto' }}>
+            <Table className={classes.table}
+                   size={dense ? 'small' : 'medium'}>
+                <EnhancedTableHead props={props}
+                                   onSelectAllClick={handleSelectAllClick}
+                                   numSelected={selection.length}
+                                   rowCount={data.items.length}/>
+                <TableBody>
+                    {rows}
+                </TableBody>
+            </Table>
+        </div>
+        {pagination}
+    </React.Fragment>;
 }
 
-export default ActionRegistry.register(withStyles(styles, { withTheme: true })(Index), {
+export default ActionRegistry.register(Index, {
     kind: ActionKind.collection,
     icon: ListIcon,
     title: 'List'
