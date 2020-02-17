a8985599602bc6bd77d8cb3e2c563ede7e1db096
Maikel Arcia Carrazana <macarci@gmail.com>
2020-02-17 00:08:59 -0300

    Add | String validator (minLength)

--- src/components/StringControl.js
+++ src/components/StringControl.js
@@ -2,8 +2,27 @@ import React from 'react';
 import InputControl from "./InputControl";
 
 function StringControl(props) {
+
+    const { minLength } = props.schema;
+
+    const validator = str => {
+        if (str) {
+            const errors = [];
+
+            if (minLength && str.length < minLength) {
+                errors.push('is too short');
+            }
+
+            if (errors.length) {
+                return errors;
+            }
+        }
+
+        return null;
+    };
+
     return (
-        <InputControl {...props}/>
+        <InputControl {...props} validator={validator}/>
     );
 }
 
