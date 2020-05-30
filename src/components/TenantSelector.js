2017483fdee90e0ec09f521e9b5a0cf12cd67831
Maikel Arcia Carrazana <macarci@gmail.com>
2020-05-30 08:43:20 -0400

    Update| Using reducer-state for tenant selector

--- src/components/TenantSelector.js
+++ src/components/TenantSelector.js
@@ -1,50 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import API from "../services/ApiService";
 import RecordSelector from "./RecordSelector";
+import reducer from "../common/reducer";
 
 const TenantTypeSelector = { namespace: '""', name: 'Account' };
 
-const TenantSelector = ({ inputClasses, onSelect, readOnly }) => {
-    const [tenant, setTenant] = useState({ name: 'Loading...', fetchCurrentTenant: true });
-    const [selection, setSelection] = useState(null);
+function TenantSelector({ inputClasses, onSelect, readOnly }) {
+    const [state, setState] = useReducer(reducer, {
+        tenant: { name: 'Loading...' }
+    });
+
+    const { tenant, selection, disabled } = state;
+
+    const selectTenant = tenant => {
+        setState({ tenant });
+        onSelect(tenant);
+    };
+
+    useEffect(() => {
+        const subscription = API.get('setup', 'user', 'me').subscribe(
+            user => user && selectTenant(user.account)
+        );
+        return () => subscription && subscription.unsubscribe();
+    }, [tenant]);
 
     useEffect(() => {
         let subscription;
         if (selection) {
-            setTenant({ name: selection.record.name, disabled: true });
+            setState({ tenant: selection.record, disabled: true });
             subscription = API.post('setup', 'user', 'me', {
                 account: {
                     id: selection.record.id
                 }
-            }).subscribe(() => selectTenant(selection.record));
+            }).subscribe(() => setState({ disabled: false }));
         }
         return () => subscription && subscription.unsubscribe();
     }, [selection]);
 
-    useEffect(() => {
-        let subscription;
-        if (tenant.fetchCurrentTenant) {
-            subscription = API.get('setup', 'user', 'me').subscribe(
-                user => user && selectTenant(user.account)
-            );
-        }
-        return () => subscription && subscription.unsubscribe();
-    }, [tenant]);
-
-    const handleSelect = selection => setSelection(selection);
-
-    const selectTenant = tenant => {
-        setTenant(tenant);
-        onSelect(tenant);
-    };
+    const handleSelect = selection => setState({ selection });
 
     return <RecordSelector dataTypeSelector={TenantTypeSelector}
                            inputClasses={inputClasses}
                            text={tenant.name}
                            onSelect={handleSelect}
-                           disabled={tenant.disabled}
+                           disabled={disabled}
                            readOnly={readOnly}
                            anchor="right"/>;
-};
+}
 
 export default TenantSelector;
