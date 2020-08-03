9dc99613f0d879c1ce8d74321ddd54971361ccba
Maikel Arcia Carrazana <macarci@gmail.com>
2020-08-02 23:41:18 -0400

    Update | File type config

--- src/config/dataTypes/Setup/FileDataType.js
+++ src/config/dataTypes/Setup/FileDataType.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import SvgIcon from "@material-ui/core/SvgIcon";
 
+const contentFields = [
+    'validators',
+    'schema_data_type'
+];
+
+const behaviorFields = [
+    'before_save_callbacks',
+    'after_save_callbacks',
+    'records_methods',
+    'data_type_methods'
+];
+
+const fields = [
+    'namespace',
+    'name',
+    'id_type',
+    'title',
+    'slug',
+    ...contentFields,
+    ...behaviorFields
+];
+
+const viewport = `{id ${fields.join(' ')}}`;
+
 export default {
     title: 'File Type',
+    groups: {
+        content: {
+            fields: contentFields
+        },
+        behavior: {
+            fields: behaviorFields
+        }
+    },
+    actions: {
+        new: {
+            fields,
+            viewport
+        },
+        edit: {
+            fields: ['id', ...fields],
+            viewport
+        }
+    },
     icon: (
         <SvgIcon>
             <path
