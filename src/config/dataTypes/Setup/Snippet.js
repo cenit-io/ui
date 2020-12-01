e31c110b173ced0c9c65b8ca99a2493a814bf86e
Maikel Arcia Carrazana <macarci@gmail.com>
2020-11-30 23:06:42 -0300

    Update | Snippet config

--- src/config/dataTypes/Setup/Snippet.js
+++ src/config/dataTypes/Setup/Snippet.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import SnippetFilledIcon from "../../../icons/SnippetFilledIcon";
+import StringCodeControl from "../../../components/StringCodeControl";
+
+const Types = {
+    text: 'plain/text',
+    ruby: 'text/x-ruby',
+    javascript: 'application/json'
+};
+
+function orchestrator({ type }, state) {
+    return {
+        code: {
+            mime: Types[type]
+        }
+    }
+}
 
 export default {
     title: 'Snippet',
@@ -7,6 +22,19 @@ export default {
     actions: {
         index: {
             fields: ['namespace', 'name', 'description', 'type', 'updated_at']
+        },
+        new: {
+            fields: ['namespace', 'name', 'description', 'type', 'code'],
+            orchestrator
+        },
+        edit: {
+            fields: ['id', 'namespace', 'name', 'description', 'type', 'code'],
+            orchestrator
+        }
+    },
+    fields: {
+        code: {
+            control: StringCodeControl
         }
     }
 };
