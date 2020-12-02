6e51ca35ca28e36927dc7767776096096322c0d0
Maikel Arcia Carrazana <macarci@gmail.com>
2020-12-02 20:24:05 -0300

    Add | Autocomplete control

---
+++ src/components/AutocompleteControl.js
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import { useFormContext } from "./FormContext";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        '& .MuiAutocomplete-endAdornment': {
+            top: theme.spacing(0.5),
+            paddingRight: theme.spacing(0.5),
+            '& .MuiAutocomplete-clearIndicator': {
+                float: 'right',
+                display: 'none',
+                '& .MuiSvgIcon-root': {
+                    fontSize: 'inherit'
+                }
+            }
+        }
+    },
+    clearable: {
+        '& .MuiAutocomplete-endAdornment': {
+            top: theme.spacing(0.5),
+            paddingRight: theme.spacing(0.5),
+            '& .MuiAutocomplete-clearIndicator': {
+                display: 'inherit',
+                visibility: 'inherit'
+            }
+        }
+    }
+}));
+
+export default function AutocompleteControl({
+                                                title,
+                                                value,
+                                                disabled,
+                                                readOnly,
+                                                error,
+                                                onChange,
+                                                options
+                                            }) {
+    const [state, setState] = useState(null);
+    const classes = useStyles();
+
+    const { initialFormValue } = useFormContext();
+
+    useEffect(() => {
+        const subscription = value.changed().subscribe(
+            v => setState(v || null)
+        );
+        value.changed().next(value.get());
+        return () => subscription.unsubscribe();
+    }, [value]);
+
+    const handleChange = (_, v) => {
+        if (v === null) {
+            const initialValue = value.valueFrom(initialFormValue);
+            if (initialValue !== undefined && initialValue !== null) {
+                value.set(null);
+            } else {
+                value.delete();
+            }
+        } else {
+            value.set(v);
+        }
+        onChange && onChange(v);
+        setState(v);
+    };
+
+    const selectableOptions = options
+        ? (options.constructor === Array
+            ? options
+            : Object.keys(options))
+        : [];
+
+    const optionLabel = key => options && options.constructor === Object
+        ? options[key]
+        : key;
+
+    return <Autocomplete options={selectableOptions}
+                         getOptionLabel={optionLabel}
+                         disabled={disabled}
+                         value={state}
+                         onChange={handleChange}
+                         renderInput={
+                             params => <TextField {...params}
+                                                  readOnly={readOnly}
+                                                  error={error}
+                                                  label={title}
+                                                  variant="filled"
+                                                  className={clsx(
+                                                      classes.root,
+                                                      state && classes.clearable
+                                                  )}/>
+                         }/>;
+}
--- src/components/EnumControl.js
+++ src/components/EnumControl.js
@@ -1,59 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-import clsx from "clsx";
-import { useFormContext } from "./FormContext";
+import React, { useEffect } from 'react';
 import { Config } from "../common/Symbols";
+import AutocompleteControl from "./AutocompleteControl";
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& .MuiAutocomplete-endAdornment': {
-            top: theme.spacing(0.5),
-            paddingRight: theme.spacing(0.5),
-            '& .MuiAutocomplete-clearIndicator': {
-                float: 'right',
-                display: 'none',
-                '& .MuiSvgIcon-root': {
-                    fontSize: 'inherit'
-                }
-            }
-        }
-    },
-    clearable: {
-        '& .MuiAutocomplete-endAdornment': {
-            top: theme.spacing(0.5),
-            paddingRight: theme.spacing(0.5),
-            '& .MuiAutocomplete-clearIndicator': {
-                display: 'inherit',
-                visibility: 'inherit'
-            }
-        }
-    }
-}));
-
-export default function EnumInput({
-                                      title,
-                                      value,
-                                      disabled,
-                                      readOnly,
-                                      error,
-                                      onChange,
-                                      property
-                                  }) {
-    const [state, setState] = useState(null);
-    const classes = useStyles();
-
-    const { initialFormValue } = useFormContext();
-
-    useEffect(() => {
-        const subscription = value.changed().subscribe(
-            v => setState(v || null)
-        );
-        value.changed().next(value.get());
-        return () => subscription.unsubscribe();
-    }, [value]);
-
+export default function EnumControl({
+                                        title,
+                                        value,
+                                        disabled,
+                                        readOnly,
+                                        error,
+                                        onChange,
+                                        property
+                                    }) {
     useEffect(() => {
         const enumOptions = property.propertySchema.enum;
         if (!enumOptions[Config]) {
@@ -65,35 +22,11 @@ export default function EnumInput({
         }
     }, [property]);
 
-    const handleChange = (_, v) => {
-        if (v === null) {
-            const initialValue = value.valueFrom(initialFormValue);
-            if (initialValue !== undefined && initialValue !== null) {
-                value.set(null);
-            } else {
-                value.delete();
-            }
-        } else {
-            value.set(v);
-        }
-        onChange && onChange(v);
-        setState(v);
-    };
-
-    return <Autocomplete options={property.propertySchema.enum}
-                         getOptionLabel={option => property.propertySchema.enum[Config][option]}
-                         disabled={disabled}
-                         value={state}
-                         onChange={handleChange}
-                         renderInput={
-                             params => <TextField {...params}
-                                                  readOnly={readOnly}
-                                                  error={error}
-                                                  label={title}
-                                                  variant="filled"
-                                                  className={clsx(
-                                                      classes.root,
-                                                      state && classes.clearable
-                                                  )}/>
-                         }/>;
+    return <AutocompleteControl options={property.propertySchema.enum[Config]}
+                                title={title}
+                                value={value}
+                                disabled={disabled}
+                                readOnly={readOnly}
+                                error={error}
+                                onChange={onChange}/>;
 }
