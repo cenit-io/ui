7c7433ef4862c7acd7db870cb4c49edf5a620d08
Maikel Arcia Carrazana <macarci@gmail.com>
2020-12-03 15:52:52 -0300

    Add | Connection data type config

--- src/config/dataTypes/Setup/Connection.js
+++ src/config/dataTypes/Setup/Connection.js
@@ -6,7 +6,18 @@ export default {
     icon: <ConnectorFilledIcon/>,
     actions: {
         index: {
-            fields: ['namespace', 'name', 'url', 'authorization', 'number', 'token', 'updated_at']
+            fields: ['namespace', 'name', 'url', 'authorization', 'updated_at']
+        },
+        new: {
+            fields: ['namespace', 'name', 'url', 'authorization', 'authorization_handler', 'parameters', 'headers', 'template_parameters']
+        }
+    },
+    groups: {
+        credentials: {
+            fields: ['authorization', 'authorization_handler']
+        },
+        parameters_and_headers: {
+            fields: ['parameters', 'headers', 'template_parameters']
         }
     }
 };
--- src/config/dataTypes/Setup/Parameter.js
+++ src/config/dataTypes/Setup/Parameter.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+const editFields = ['key', 'value', 'description', 'metadata'];
+
 export default {
     title: 'Parameter',
     actions: {
         index: {
             fields: ['key', 'value', 'updated_at']
+        },
+        new: {
+            fields: editFields,
+            seed: {
+                metadata: {}
+            }
+        },
+        edit: {
+            fields: editFields
         }
     }
 };
