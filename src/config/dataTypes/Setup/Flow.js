71784d3aeca6a378ed68ee9e3f877b5339f0ee54
Maikel Arcia Carrazana <macarci@gmail.com>
2021-01-04 10:55:44 -0300

    Fix | Flow form config

--- src/config/dataTypes/Setup/Flow.js
+++ src/config/dataTypes/Setup/Flow.js
@@ -10,10 +10,30 @@ export default {
     actions: {
         index: {
             fields: ['namespace', 'name', 'description', 'active', 'event', 'translator', 'updated_at']
+        },
+        new: {
+            fields: [
+                'namespace', 'name', 'description', 'event', 'translator',
+                'custom_data_type', 'data_type_scope', 'scope_filter', 'scope_evaluator', 'lot_size',
+                'webhook', 'authorization', 'connection_role', 'before_submit',
+                'response_translator', 'response_data_type', 'discard_events',
+                'notify_request', 'notify_response',
+                'active', 'auto_retry', 'after_process_callbacks'
+            ]
         }
     },
     formControl: FlowFormControl,
     fields: {
+        event: {
+            controlProps: {
+                editDisabled: true
+            }
+        },
+        translator: {
+            controlProps: {
+                editDisabled: true
+            }
+        },
         data_type_scope: {
             control: AutocompleteControl
         },
@@ -34,7 +54,6 @@ export default {
             ]
         },
         behavior: {
-            title: 'Request & Response',
             fields: ['active', 'auto_retry', 'after_process_callbacks']
         }
     }
