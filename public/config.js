7029479fe64c2bdd45ef59d5798e85145d67100d
Maikel Arcia Carrazana <macarci@gmail.com>
2021-01-08 10:52:50 -0300

    Update | Config environment for upgrading transition testing

--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.appConfig = {
     useEnvironmentConfig: false,
     localhost: 'http://cenit-admin.s3-website-us-west-2.amazonaws.com',
-    cenitHost: 'https://cenit.io',
+    cenitHost: 'http://ec2-18-208-253-0.compute-1.amazonaws.com',
     timeoutSpan: 300000,
     appIdentifier: 'admin'
 };
