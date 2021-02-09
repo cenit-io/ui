0c5b8f705f29dda208120b1a521811e807737e2e
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-08 22:25:32 -0300

    Add | Sudo action (pilot)

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,38 +1,39 @@
-import { Avatar, CircularProgress, Button, Typography, makeStyles } from "@material-ui/core";
-import ExitIcon from '@material-ui/icons/ExitToApp';
+import { Avatar, CircularProgress, Typography, makeStyles, ListItemText, ListItem } from "@material-ui/core";
+import SudoIcon from '@material-ui/icons/SupervisorAccountTwoTone';
 import React from "react";
 import AuthorizationService from "../services/AuthorizationService";
+import List from "@material-ui/core/List";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import { useTenantContext } from "../layout/TenantContext";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const LogoutIcon = props => (
+    <SvgIcon {...props}>
+        <path
+            d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z"/>
+    </SvgIcon>
+);
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        padding: `${theme.spacing(1)}px`,
-        display: 'flex',
-        flexDirection: 'column'
-    },
-    profile: {
-        display: 'flex',
-        justifyContent: 'space-between'
-    },
     avatarContainer: {
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        margin: theme.spacing(1),
+        minWidth: theme.spacing(26)
     },
     avatar: {
         width: `${theme.spacing(8)}px`,
         height: `${theme.spacing(8)}px`,
     },
-    profileData: {
-        flexGrow: 1,
-        flexDirection: 'column',
-        textAlign: 'right',
-        marginLeft: `${theme.spacing(1)}px`,
-        wordBreak: 'break-word',
-        maxWidth: `${theme.spacing(20)}px`
+    danger: {
+        color: theme.palette.error.main
     },
-    logout: {
-        display: 'flex',
-        justifyContent: 'flex-end'
+    safe: {
+        color: theme.palette.success.main
+    },
+    warn: {
+        color: theme.palette.warning.main
     }
 }));
 
@@ -40,39 +41,61 @@ const UserCard = ({ idToken, onClose }) => {
 
     const classes = useStyles();
 
+    const [tenantState, setTenantState] = useTenantContext();
+
+    const { user } = tenantState;
+
+    const isSuperUser = !!(user.roles || []).find(({ name }) => name === 'super_admin');
+
     if (!idToken) return <CircularProgress/>;
 
     const avatar = <Avatar alt={idToken.name} src={idToken.picture} className={classes.avatar}/>;
 
-    function logout() {
+    const logout = () => {
         onClose && onClose();
         AuthorizationService.logout();
+    };
+
+    let sudoControl;
+    if (isSuperUser) {
+        const superEnabled = user.super_admin_enabled;
+        const action = superEnabled ? 'Disable' : 'Enable';
+        sudoControl = (
+            <ListItem button onClick={() => setTenantState({ switchSudo: true })}>
+                <ListItemIcon className={superEnabled ? classes.safe : classes.warn}>
+                    <SudoIcon/>
+                </ListItemIcon>
+                <ListItemText primary={`${action} super user`}/>
+            </ListItem>
+        );
     }
 
-    return <div className={classes.root}>
-        <div className={classes.profile}>
-            <div className={classes.avatarContainer}>
-                {avatar}
+    return (
+        <div className="flex column">
+            <div className={classes.profile}>
+                <div className={classes.avatarContainer}>
+                    {avatar}
+                </div>
+                <div className="flex column align-items-center">
+                    <Typography component="div" variant="subtitle2">
+                        {idToken.name}
+                    </Typography>
+                    <Typography component="div" variant="caption">
+                        {idToken.email}
+                    </Typography>
+                </div>
             </div>
-            <div className={classes.profileData}>
-                <Typography component="div" variant="subtitle2">
-                    {idToken.name}
-                </Typography>
-                <Typography component="div" variant="caption">
-                    {idToken.email}
-                </Typography>
-            </div>
+            <List>
+                {sudoControl}
+                <ListItem button onClick={logout}>
+                    <ListItemIcon className={classes.danger}>
+                        <LogoutIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary="Logout"/>
+                </ListItem>
+            </List>
         </div>
-        <div className={classes.logout}>
-            <Button variant="contained"
-                    color="secondary"
-                    size="small"
-                    onClick={logout}>
-                Logout
-                <ExitIcon/>
-            </Button>
-        </div>
-    </div>;
+    );
 };
 
 export default UserCard;
--- src/layout/TenantContext.js
+++ src/layout/TenantContext.js
@@ -5,6 +5,7 @@ import FrezzerLoader from "../components/FrezzerLoader";
 import { eq } from "../services/BLoC";
 import ConfigService from "../services/ConfigService";
 import makeStyles from "@material-ui/core/styles/makeStyles";
+import { Status } from "../common/Symbols";
 
 const TC = React.createContext({});
 
@@ -21,20 +22,37 @@ const useLoaderStyles = makeStyles(() => ({
 
 export default function TenantContext({ children }) {
     const [state, setState] = useSpreadState({
-        loading: true
+        loading: true,
+        switchSudo: false
     });
 
     const classes = useLoaderStyles();
 
-    const { tenant, switchingTenant, loading } = state;
+    const { tenant, switchingTenant, loading, switchSudo } = state;
 
-    const setSwitchingTenant = switchingTenant => setState({ switchingTenant });
+    useEffect(() => {
+        if (switchSudo) {
+            setState({ loading: true });
+            const subscription = API.get('setup', 'user', 'me', 'digest', 'switch_sudo').subscribe(
+                user => {
+                    if (user[Status] === 200) {
+                        setState({ user, switchSudo: false, loading: false });
+                    } else {
+                        setState({ switchSudo: false, loading: false })
+                    }
+                }
+            );
+
+            return () => subscription.unsubscribe();
+        }
+    }, [switchSudo]);
 
     useEffect(() => {
         const subscription = API.get('setup', 'user', 'me').subscribe(
             user => {
                 if (user) {
                     setState({
+                        user,
                         tenant: user.account,
                         switchingTenant: null,
                         loading: false
