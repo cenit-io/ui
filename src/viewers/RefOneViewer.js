f0341a74d86eab8ac9b7b2a7839a60aa9cc1eaf9
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-17 21:37:32 -0300

    Update | Reusing ref-one viewer with custom ref data type

--- src/viewers/RefOneViewer.js
+++ src/viewers/RefOneViewer.js
@@ -1,21 +1,22 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Skeleton from "@material-ui/lab/Skeleton";
 import Chip from "@material-ui/core/Chip";
 import { RecordSubject, TabsSubject } from "../services/subjects";
 import { of } from "rxjs";
 import { switchMap } from "rxjs/operators";
-import spreadReducer from "../common/spreadReducer";
 import zzip from "../util/zzip";
+import { useSpreadState } from "../common/hooks";
 
-export default function RefOneViewer({ prop, value, className }) {
+export default function RefOneViewer({ prop, value, className, refDataType, variant, color }) {
 
-    const [state, setState] = useReducer(spreadReducer, {});
+    const [state, setState] = useSpreadState();
 
     const { title, dataType } = state;
 
     useEffect(() => {
-        if (prop && value) {
+        if ((refDataType || prop) && value) {
             const subscription = (
+                (refDataType && of(refDataType)) ||
                 ((!value._type || value._type === prop.dataType.type_name()) && of(prop.dataType)) ||
                 prop.dataType.findByName(value._type)
             ).pipe(
@@ -26,13 +27,17 @@ export default function RefOneViewer({ prop, value, className }) {
 
             return () => subscription.unsubscribe();
         }
-    }, [prop, value]);
+    }, [refDataType, prop, value]);
 
     const handleClick = () => TabsSubject.next(RecordSubject.for(dataType.id, value.id).key);
 
     if (value) {
         if (title) {
-            return <Chip label={title} onClick={handleClick} className={className}/>;
+            return <Chip label={title}
+                         onClick={handleClick}
+                         className={className}
+                         variant={variant}
+                         color={color}/>;
         }
 
         return <Skeleton variant="text"/>;
