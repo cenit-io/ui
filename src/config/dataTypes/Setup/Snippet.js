371c49080a2c87e9c437e958d475ca033867777f
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-17 21:48:57 -0300

    Add | Snippets shared config

--- src/config/dataTypes/Setup/Snippet.js
+++ src/config/dataTypes/Setup/Snippet.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SnippetFilledIcon from "../../../icons/SnippetFilledIcon";
 import StringCodeControl from "../../../components/StringCodeControl";
+import mergeOrchestrators from "../../orchestrators/mergeOrchestrators";
+import sharedOriginFields from "../../orchestrators/sharedOriginFields";
 
 const Types = {
     text: 'plain/text',
@@ -8,15 +10,19 @@ const Types = {
     javascript: 'application/json'
 };
 
-function orchestrator({ type }, state) {
-    if (state.code?.mime !== type) {
-        return {
-            code: {
-                mime: Types[type]
+const fields = ['namespace', 'name', 'description', 'type', 'code'];
+
+const orchestrator = mergeOrchestrators(
+    ({ type }, state) => {
+        const mime = Types[type];
+        if (state.code?.mime !== mime) {
+            return {
+                code: { mime }
             }
         }
-    }
-}
+    },
+    sharedOriginFields(...fields)
+);
 
 export default {
     title: 'Snippet',
@@ -26,7 +32,10 @@ export default {
             fields: ['namespace', 'name', 'description', 'type', 'updated_at']
         },
         new: {
-            fields: ['namespace', 'name', 'description', 'type', 'code']
+            fields
+        },
+        edit: {
+            viewport: `{id ${fields.join(' ')} origin}`
         }
     },
     fields: {
