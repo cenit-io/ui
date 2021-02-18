62701599d1360fcf2e4b40238113e0e1126e11d7
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-18 20:04:27 -0300

    Add | Filled and outlined variants for input controls

--- src/components/InputControl.js
+++ src/components/InputControl.js
@@ -3,6 +3,8 @@ import { FormControl, IconButton, InputAdornment, InputLabel } from "@material-u
 import ClearIcon from "@material-ui/icons/Clear";
 import FilledInput from "@material-ui/core/FilledInput";
 import reactiveControlFor from "./reactiveControlFor";
+import Input from "@material-ui/core/Input";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
 
 const InputControl = reactiveControlFor(
     ({
@@ -17,31 +19,45 @@ const InputControl = reactiveControlFor(
          autoFocus,
          onFocus,
          onClear,
-         type
-     }) => (
-        <FormControl variant="filled" fullWidth={true} disabled={disabled}>
-            <InputLabel>{title}</InputLabel>
-            <FilledInput key={dynamicKey}
-                         type={type}
-                         readOnly={readOnly}
-                         error={error}
-                         defaultValue={value}
-                         onChange={e => onChange(e.target.value)}
-                         onBlur={onBlur}
-                         autoFocus={autoFocus}
-                         onFocus={onFocus}
-                         endAdornment={
-                             !readOnly && !disabled && value !== undefined && value !== null &&
-                             <InputAdornment position="end">
-                                 <IconButton onClick={onClear}>
-                                     <ClearIcon/>
-                                 </IconButton>
-                             </InputAdornment>
-                         }
-                         variant='filled'
-            />
-        </FormControl>
-    )
+         type,
+         multiline,
+         variant
+     }) => {
+        if (variant !== 'outlined') {
+            variant = 'filled';
+        }
+
+        const InputControl = variant === 'outlined'
+            ? OutlinedInput
+            : FilledInput;
+
+        return (
+            <FormControl fullWidth disabled={disabled}>
+                <InputLabel htmlFor={dynamicKey} variant={variant}>{title}</InputLabel>
+                <InputControl key={dynamicKey}
+                              id={dynamicKey}
+                              label={title}
+                              type={type}
+                              readOnly={readOnly}
+                              error={error}
+                              defaultValue={value}
+                              onChange={e => onChange(e.target.value)}
+                              onBlur={onBlur}
+                              autoFocus={autoFocus}
+                              onFocus={onFocus}
+                              multiline={multiline}
+                              endAdornment={
+                                  !readOnly && !disabled && value !== undefined && value !== null &&
+                                  <InputAdornment position="end">
+                                      <IconButton onClick={onClear}>
+                                          <ClearIcon/>
+                                      </IconButton>
+                                  </InputAdornment>
+                              }
+                              variant='filled'/>
+            </FormControl>
+        );
+    }
 );
 
 export default InputControl;
