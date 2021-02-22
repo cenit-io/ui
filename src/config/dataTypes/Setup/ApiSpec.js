fd9009591e9736b51e6d4b727d195a8ab097956a
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-22 17:09:39 -0300

    Add | API specs shared config

--- src/config/dataTypes/Setup/ApiSpec.js
+++ src/config/dataTypes/Setup/ApiSpec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ApiSpecsFilledIcon from "../../../icons/ApiSpecsFilledIcon";
 import StringCodeControl from "../../../components/StringCodeControl";
+import sharedOriginFields from "../../orchestrators/sharedOriginFields";
+
+const fields = ['title', 'url', 'specification'];
 
 export default {
     title: 'API Spec',
@@ -10,10 +13,13 @@ export default {
             fields: ['title', 'url', 'updated_at']
         },
         new: {
-            fields: ['title', 'url', 'specification'],
+            fields,
             seed: {
                 specification: "swagger: '2.0'\ninfo:\n  version: 0.0.1\n  title: title\n  description: description\n  termsOfService: terms\n  contact:\n    name: name\n    url: http://example\n    email: email@example\n  license:\n    name: MIT\n    url: http://opensource.org/licenses/MIT\npaths:\n  /resource:\n    get:\n     responses:\n      200:\n        description: \"Successful operation\"\n        schema:\n          type: object\n      404:\n        description: \"Resource not found\"\n"
             }
+        },
+        edit: {
+            viewportFields: [...fields, 'origin']
         }
     },
     fields: {
@@ -23,5 +29,6 @@ export default {
                 mime: 'text/x-yaml'
             }
         }
-    }
+    },
+    orchestrator: sharedOriginFields(...fields)
 };
