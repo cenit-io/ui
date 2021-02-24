cab7c5ee1a4d4b3fca93175b24b6bf5a1d474630
Maikel Arcia Carrazana <macarci@gmail.com>
2021-02-24 12:27:01 -0300

    Fix | Authorization clients config

--- src/config/dataTypes/Setup/AuthorizationClient.js
+++ src/config/dataTypes/Setup/AuthorizationClient.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import AuthorizationClientsFilledIcon from "../../../icons/AuthorizationClientsFilledIcon";
-import ApiSpecsFilledIcon from "../../../icons/ApiSpecsFilledIcon";
 import itemLabelFor from "../../itemLabelFor";
 
 const title = 'Authorization Client';
@@ -12,7 +11,7 @@ export default {
     actions: {
         index: {
             fields: ['_type', 'provider', 'name', 'updated_at'],
-            viewport: '{_type provider {namespace name} name updated_at}'
+            viewport: '{id _type provider {namespace name} name updated_at origin}'
         }
     }
 };
