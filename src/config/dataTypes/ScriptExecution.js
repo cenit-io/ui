58fd904caa3220b2f4c6666958955e75eecdcb25
Maikel Arcia Carrazana <macarci@gmail.com>
2021-03-09 23:25:45 -0300

    Fix | Title

--- src/config/dataTypes/ScriptExecution.js
+++ src/config/dataTypes/ScriptExecution.js
@@ -2,7 +2,7 @@ import React from 'react';
 import commonTaskConfig from "./Setup/commonTaskConfig";
 import ViewerControl from "../../components/ViewerControl";
 
-const ScriptExecution = commonTaskConfig('API Spec Import', {
+const ScriptExecution = commonTaskConfig('Script Execution', {
     script: {
         control: ViewerControl
     }
