068d058fc6374e71bb94499f66cc2f10b3b69315
Maikel Arcia Carrazana <macarci@gmail.com>
2021-07-05 14:55:03 -0400

    Update | Using env config by default

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.appConfig = {
-    useEnvironmentConfig: false,
+    useEnvironmentConfig: true,
     localhost: 'http://cenit-admin.s3-website-us-west-2.amazonaws.com',
     cenitHost: 'http://ec2-18-208-253-0.compute-1.amazonaws.com',
     timeoutSpan: 300000,
