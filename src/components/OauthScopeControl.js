b5002ef7171b4cde6b655995015f25e663fb0b6b
Maikel Arcia Carrazana <macarci@gmail.com>
2021-11-12 10:48:46 -0300

    Update | Using autocomplete control for OAuth scope control

--- src/components/OauthScopeControl.js
+++ src/components/OauthScopeControl.js
@@ -1,85 +1,71 @@
-import { Chip, TextField, useTheme } from "@material-ui/core";
-import { Autocomplete } from "@material-ui/lab";
-import React, { useEffect, useState } from "react";
+import { Chip, makeStyles } from "@material-ui/core";
+import React from "react";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+import orange from "@material-ui/core/colors/orange";
+import blue from "@material-ui/core/colors/blue";
+import red from "@material-ui/core/colors/red";
+import AutocompleteControl from "./AutocompleteControl";
 
-const scopes = [
-  { title: "auth", value: "auth", color: "#D5C8C8" },
-  { title: "session_access", value: "session_access", color: "#D5C8C8" },
-  { title: "offline_access", value: "offline_access", color: "#D5C8C8" },
-  { title: "multi_tenant", value: "multi_tenant", color: "#D5C8C8" },
-  { title: "openid", value: "openid", color: "#D5C8C8" },
-  { title: "email", value: "email", color: "#D5C8C8" },
-  { title: "profile", value: "profile", color: "#D5C8C8" },
-  { title: "create", value: "create", color: "#5BC0DE" },
-  { title: "read", value: "read", color: "#5CB85C" },
-  { title: "update", value: "update", color: "#F0AD4E" },
-  { title: "delete", value: "delete", color: "#D9534F" },
-  { title: "digest", value: "digest", color: "#663399" },
+const SCOPES = [
+    'auth',
+    'session_access',
+    'offline_access',
+    'multi_tenant',
+    'openid',
+    'profile',
+    'email',
+    'create',
+    'read',
+    'update',
+    'delete',
+    'digest'
 ];
 
-function OauthScopeControl(props) {
-  const [val, setValue] = useState([]);
-  const [valueToSave, setValueToSave] = useState("");
-  const theme = useTheme();
-  const scopeValues = props.value.parent.cache.scope;
-  const { readOnly, title, value } = props;
+const COLORS = {
+    create: blue[200],
+    read: green[200],
+    update: orange[200],
+    delete: red[200],
+    digest: purple[200],
+};
 
-  useEffect(() => {
-    let result = [];
-    function startsWith(element) {
-      return this.includes(element.value);
-    }
+const useStyles = makeStyles(theme => Object.keys(COLORS).reduce(
+    (classes, scope) => ({
+        ...classes,
+        [scope]: {
+            color: theme.palette.getContrastText(COLORS[scope]),
+            backgroundColor: COLORS[scope],
+        }
+    }), {}
+));
 
-    if (scopeValues) {
-      result = scopes.filter(startsWith, scopeValues);
-    }
+export default function OauthScopeControl(props) {
 
-    setValue(result);
-  }, [scopeValues]);
+    const classes = useStyles();
 
-  useEffect(() => {
-    setValueToSave(val.map((v) => v.title).join(" "));
-  }, [val, valueToSave]);
+    const { scopes, readOnly, disabled } = props;
 
-  useEffect(() => {
-    value.set(valueToSave);
-  }, [value, valueToSave]);
+    const renderTags = (tagValue, getTagProps) =>
+        tagValue.map((option, index) => {
+                const tagProps = getTagProps({ index });
+                if (readOnly) {
+                    tagProps.onDelete = undefined;
+                }
+                return <Chip classes={{ root: classes[option] }}
+                             label={option.toUpperCase()}
+                             {...tagProps}
+                             disabled={disabled}/>;
+            }
+        );
 
-  const onChangeAutocomplete = (event, newValue) => {
-    setValue([...newValue]);
-  };
-
-  const renderTagsAutocomplete = (tagValue, getTagProps) =>
-    tagValue.map((option, index) => (
-      <Chip
-        style={{
-          backgroundColor: option.color,
-          color: theme.palette.getContrastText(option.color),
-        }}
-        label={option.title.toUpperCase()}
-        {...getTagProps({ index })}
-      />
-    ));
-
-  return (
-    <Autocomplete
-      multiple
-      id="scopes-tags"
-      value={val}
-      disabled={readOnly}
-      onChange={onChangeAutocomplete}
-      options={scopes}
-      getOptionLabel={(option) => option.title}
-      renderTags={renderTagsAutocomplete}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label={title}
-          placeholder={readOnly ? "" : "Add scope"}
-        />
-      )}
-    />
-  );
+    return (
+        <AutocompleteControl {...props}
+                             options={scopes || SCOPES}
+                             multiple={true}
+                             renderTags={renderTags}
+                             formatter={values => values?.join(' ')}
+                             parser={value => value?.split(' ')}
+                             disabled={disabled || readOnly}/>
+    );
 }
-
-export default OauthScopeControl;
