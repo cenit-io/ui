f1aea2195dacf8fff789263a3526b61acfbac6b6
Miguel Sancho <sanchojaf@gmail.com>
2026-02-27 16:10:33 -0500

    feat(auth): align UI OAuth scope with setup model contract

--- src/util/request.ts
+++ src/util/request.ts
@@ -7,9 +7,12 @@ import {
   apiRequest as _apiRequest,
 } from "cenit-frontend-sdk/request";
 
+const OAUTH_SCOPE =
+  'openid profile email offline_access session_access multi_tenant read update delete digest ' +
+  'openid create {"namespace":"Setup","name":{"$in":["DataType","JsonDataType","Snippet","Template","LiquidTemplate","Flow","PlainWebhook","Collection","Application"]}}';
+
 export const authorize = () => {
-  const scope = 'openid profile email offline_access session_access multi_tenant create read update delete digest';
-  return _authorize(scope);
+  return _authorize(OAUTH_SCOPE);
 }
 
 export const authWithAuthCode = (authCode: string) => from(_authWithAuthCode(authCode));
